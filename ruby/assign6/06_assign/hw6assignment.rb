0560c89290721ee8116d34c7cfa6bdcc5aa7caab
shoya <shoya@uvic.ca>
2021-03-24 20:32:01 -0700

    upload content for assignment 6

---
+++ ruby/assign6/06_assign/hw6assignment.rb
@@ -0,0 +1,24 @@
+# Programming Languages, Homework 6, hw6runner.rb
+
+# This is the only file you turn in,
+# so do not modify the other files as
+# part of your solution.
+
+class MyPiece < Piece
+  # The constant All_My_Pieces should be declared here:
+
+  # Your Enhancements here
+
+end
+
+class MyBoard < Board
+  # Your Enhancements here:
+
+end
+
+class MyTetris < Tetris
+  # Your Enhancements here:
+
+end
+
+
